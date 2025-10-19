e2606574954f8c98c182dbfd3ca9cb2c1e61715f
AlexDvor <alexmatvichuk@gmail.com>
2025-10-19 10:13:53 +0200

    Add Task

---
+++ src/js/1-timer.js
@@ -0,0 +1,8 @@
+const refs = {
+  inputPicker: document.querySelector('#datetime-picker'),
+  startBtn: document.querySelector('button[data-start]'),
+  valueDays: document.querySelector('.value[data-days]'),
+  valueHours: document.querySelector('.value[data-hours]'),
+  valueMinutes: document.querySelector('.value[data-minutes]'),
+  valueSeconds: document.querySelector('.value[data-seconds]'),
+};
